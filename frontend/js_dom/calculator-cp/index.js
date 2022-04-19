08431443b08a81701536f907fab33a4546f4af1a
Azzam Nur Alwi .M <101518774+azzmnrwebdev@users.noreply.github.com>
2022-04-19 08:27:39 +0700

    calculator-cp task code change

--- frontend/js_dom/calculator-cp/index.js
+++ frontend/js_dom/calculator-cp/index.js
@@ -1,21 +1,44 @@
-//handle when the number is pressed. It renders the number into #input element
+// handle when the number is pressed. It renders the number into #input element
 function number(number) {
   // TODO: answer here
+  let input = document.getElementById("input");
+  if (input.value == "0") {
+    input.value = number;
+  } else {
+    input.value += number;
+  }
 }
 
-//handle when the Del button, AC button and other operators (+, /, -, x) are pressed. It renders the operator into #input element
+// handle when the Del button, AC button and other operators (+, /, -, x) are pressed. It renders the operator into #input element
 function operator(operator) {
   let input = document.getElementById("input");
   if (operator == "Del") {
     // TODO: answer here
+    input.value = input.value.slice(0, -1);
   } else if (operator == "AC") {
     // TODO: answer here
-  } else {
-    // TODO: answer here
+    input.value = "";
+  } else if (operator === "+") {
+    input.value += "+";
+  } else if (operator === "-") {
+    input.value += "-";
+  } else if (operator === "*") {
+    input.value += "*";
+  } else if (operator === "/") {
+    input.value += "/";
+  } else if (operator === ".") {
+    input.value += ".";
   }
 }
 
-//handle when = button is pressed. It renders the result into #input element
+// handle when = button is pressed. It renders the result into #input element
 function calculate() {
   // TODO: answer here
+  let input = document.getElementById("input");
+  try {
+    input.value = eval(input.value);
+  } catch (e) {
+    (input.value = "0"), (input.value = "0");
+    input.value = eval(input.value);
+  }
 }
