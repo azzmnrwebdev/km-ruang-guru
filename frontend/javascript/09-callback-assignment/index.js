e2aa139cd3cdb98116fabf061203f181436472a2
Azzam Nur Alwi .M <101518774+azzmnrwebdev@users.noreply.github.com>
2022-04-20 15:41:32 +0700

    09-callback-assignment code change

--- frontend/javascript/09-callback-assignment/index.js
+++ frontend/javascript/09-callback-assignment/index.js
@@ -1,7 +1,8 @@
 /**
  * Lengkapilah function dibawah ini dimana terdapat function groupBy yang mengelompokkan elemen dari array dengan nilai yang dikembalikan dari callback function (isOdd) melalui argumen.
+ *
  * isOdd sendiri merupakan sebuah function yang akan melakukan validasi apakah tahun kelahiran dari data ganjil atau genap, Nilai yang dikembalikan berupa true atau false.
- * 
+ *
  * input:
  * [
  *    {
@@ -20,8 +21,8 @@
  *      place: 'Bogor',
  *    },
  *  ]
- * 
- * output: 
+ *
+ * output:
  *  { false:
  *    [ { name: 'Dito', year: 1992, place: 'Bogor' },
  *      { name: 'Uli', year: 1996, place: 'Bogor' } ],
@@ -30,35 +31,47 @@
 
 const input = [
   {
-    name: 'Adit',
-    year: 1996,
-    place: 'Bogor',
+    name: "Dito",
+    year: 1992,
+    place: "Bogor",
   },
   {
-    name: 'Fauzan',
+    name: "Tegar",
     year: 1995,
-    place: 'Depok',
+    place: "Depok",
   },
   {
-    name: 'Vika',
-    year: 1990,
-    place: 'Bandung',
+    name: "Uli",
+    year: 1996,
+    place: "Bogor",
   },
 ];
 
 const isOdd = (data) => {
   // TODO: answer here
+  if (data.year % 2 !== 0) {
+    return true; // ganjil
+  } else {
+    return false; // genap
+  }
 };
 
-const groupBy = (array, callback) => array.reduce(
-  (accumulator, dataValue) => {
+const groupBy = (array, callback) =>
+  array.reduce((accumulator, dataValue) => {
     // TODO: answer here
-  }, {}
-);
+    const key = callback(dataValue);
+    if (!accumulator[key]) {
+      accumulator[key] = [dataValue];
+    } else {
+      accumulator[key].push(dataValue);
+    }
 
-console.log(groupBy(input, isOdd))
+    return accumulator;
+  }, {});
+
+console.log(groupBy(input, isOdd));
 
 module.exports = {
   groupBy,
-  isOdd
-}
+  isOdd,
+};
