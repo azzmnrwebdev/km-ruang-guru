0d6d733e739e35e1ec824ffac61094807ff8a412
azzmnrwebdev <azzmnrwebdev@gmail.com>
2022-04-28 06:31:40 +0700

    parentheses-validation assignment code change

--- frontend/data-structure/assignment/parentheses-validation/stack.js
+++ frontend/data-structure/assignment/parentheses-validation/stack.js
@@ -1,33 +1,33 @@
 module.exports = class Stack {
-    constructor() {
-        this.data = []
-        this.top = -1
-    }
+  constructor() {
+    this.data = [];
+    this.top = -1;
+  }
 
-    push(elemen) {
-        if (this.data.length == this.size) {
-            throw "stack overflow"
-        } else {
-            this.top += 1
-            return this.data.push(elemen)
-        }
+  push(elemen) {
+    if (this.data.length == this.size) {
+      throw "stack overflow";
+    } else {
+      this.top += 1;
+      return this.data.push(elemen);
     }
+  }
 
-    pop() {
-        if (this.top == -1) {
-            throw "stack underflow"
-        } else {
-            let poppedValue = this.data.pop()
-            this.top -= 1
-            return poppedValue
-        }
+  pop() {
+    if (this.top == -1) {
+      throw "stack underflow";
+    } else {
+      let poppedValue = this.data.pop();
+      this.top -= 1;
+      return poppedValue;
     }
+  }
 
-    peek() {
-        if (this.top == -1) {
-            throw "stack is empty"
-        } else {
-            return this.data[this.top]
-        }
+  peek() {
+    if (this.top == -1) {
+      throw "stack is empty";
+    } else {
+      return this.data[this.top];
     }
-}
+  }
+};
