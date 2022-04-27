3c190c15c73a05df4f5ba887d1bf9eaa04689182
azzmnrwebdev <azzmnrwebdev@gmail.com>
2022-04-28 06:34:28 +0700

    text-editor task code change

--- frontend/data-structure/assignment/text-editor/index.js
+++ frontend/data-structure/assignment/text-editor/index.js
@@ -1,21 +1,41 @@
 module.exports = class TextEditor {
-    constructor() {
-        // TODO: answer here
-    }
+  constructor() {
+    // TODO: answer here
+    this.data = [];
+    this.top = -1;
+    this.undoStack = [];
+    this.redoStack = [];
+  }
 
-    write(c) {
-        // TODO: answer here
-    }
+  write(c) {
+    // TODO: answer here
+    this.top += 1;
+    this.undoStack.push(c);
+    return this.data.push(c);
+  }
 
-    read() {
-        // TODO: answer here
-    }
+  read() {
+    // TODO: answer here
+    return this.data.join("");
+  }
 
-    undo() {
-        // TODO: answer here
+  undo() {
+    // TODO: answer here
+    if (this.data.length !== 0) {
+      let out = this.undoStack.pop();
+      this.redoStack.push(out);
+      this.data.pop();
+      this.top -= 1;
     }
-    
-    redo() {
-        // TODO: answer here
+  }
+
+  redo() {
+    // TODO: answer here
+    if (this.redoStack.length !== 0) {
+      let inp = this.redoStack.pop();
+      this.top += 1;
+      this.data.push(inp);
+      this.undoStack.push(inp);
     }
-}
+  }
+};
