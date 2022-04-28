713ef3639d91532b1547a77c35d6f03a12670950
azzmnrwebdev <azzmnrwebdev@gmail.com>
2022-04-28 22:47:47 +0700

    3-array-cp assignment code change

--- frontend/data-structure/array/3-array-cp/index.js
+++ frontend/data-structure/array/3-array-cp/index.js
@@ -1,12 +1,12 @@
 // Kalimat adalah daftar kata yang dipisahkan oleh satu spasi tanpa spasi awal atau akhir.
 // Kita diberikan serangkaian string dengan nama sentences, di mana setiap sentences[i] mewakili satu kalimat.
 // Kembalikan jumlah maksimum kata yang muncul dalam satu kalimat.
-//
+
 // Contoh 1
 // Input: sentences = ["Andi suka bermain bola", "Saya sedang belajar struktur data", "Terima kasih"]
 // Output: 5
 // Explanation: jumlah kata terbanyak ada di "Saya sedang belajar struktur data", karena terdapat 5 kata dibandingkan kalimat lainnya, 4 dan 2 kata
-//
+
 // Contoh 2
 // Input: sentences = ["Andi suka bermain bola", "Budi suka bermain basket", "Terima kasih"]
 // Output: 4
@@ -14,18 +14,27 @@
 // Ada kemungkinan bahwa beberapa kalimat mengandung jumlah kata yang sama.
 
 function mostWordsFound(sentences) {
-    let max = 0;
-    for (let i = 0; i < sentences.length; i++) {
-        let count = countWords(sentences[i]);
-        // TODO: answer here
+  let max = 0;
+  for (let i = 0; i < sentences.length; i++) {
+    let count = countWords(sentences[i]);
+    // TODO: answer here
+    if (count > max) {
+      max = count;
     }
-    return max
+  }
+  return max;
 }
 
 function countWords(sentence) {
-    return 0; // TODO: replace this
+  let count = 0;
+  let words = sentence.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    count++;
+  }
+  return count; // TODO: replace this
 }
 
 module.exports = {
-    mostWordsFound, countWords
-}
+  mostWordsFound,
+  countWords,
+};
