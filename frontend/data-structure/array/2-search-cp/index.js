bfe2b6ffada57634dd3c06ec5d8e71eb028f1033
azzmnrwebdev <azzmnrwebdev@gmail.com>
2022-04-28 22:55:34 +0700

    2-search-cp task code change

--- frontend/data-structure/array/2-search-cp/index.js
+++ frontend/data-structure/array/2-search-cp/index.js
@@ -1,6 +1,16 @@
 function searchMatch(arr1, arr2) {
-    let result = [];
-    return result // TODO: replace this
+  let result = [];
+  let newArr = arr1.concat(arr2);
+
+  for (let i = 0; i < newArr.length; i++) {
+    for (let j = i+1; j < newArr.length; j++) {
+      if (newArr[i] == newArr[j]) {
+        result.push(newArr[j])
+      }
+    }
+  }
+
+  return result; // TODO: replace this
 }
 
 let cars1 = ["Toyota", "Honda", "Nissan", "BMW", "Chevy", "Ford"];
